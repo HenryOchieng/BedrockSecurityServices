fad2796bea25e3acb4d47c822d3dd2e08ddc9b4e
Henry1886 <henryochieng8@gmail.com>
2021-05-04 04:20:01 +0300

    project update

--- Bedrock/settings.py
+++ Bedrock/settings.py
@@ -128,7 +128,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'base/static'),
 )
 
 # Default primary key field type
